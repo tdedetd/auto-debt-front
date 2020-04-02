0eabab61bac65c98116921278d84fe862e267fe2
tdedetd <tdedetd@gmail.com>
2020-04-03 00:34:31 +0300

    fix mock for checks

--- src/app/const/mock/check-list-credit.ts
+++ src/app/const/mock/check-list-credit.ts
@@ -4,43 +4,55 @@ export const CHECK_LIST_CREDIT: Check[] = [
   {
     id: 1,
     name: 'Лента',
+    paidBy: 1,
     date: '2020-01-16T12:25:00.000Z',
-    sum: 181,
+    sumTotal: 181,
+    sumIndividual: 181,
     status: 'accepted'
   },
   {
     id: 2,
     name: 'Лента',
+    paidBy: 1,
     date: '2020-01-15T21:03:00.000Z',
-    sum: 1034,
+    sumTotal: 1034,
+    sumIndividual: 1034,
     status: 'accepted'
   },
   {
     id: 3,
     name: 'Очень длинное название',
+    paidBy: 1,
     date: '2020-01-14T15:28:00.000Z',
-    sum: 12.35,
+    sumTotal: 12.35,
+    sumIndividual: 12.35,
     status: 'accepted'
   },
   {
     id: 4,
     name: 'Теремок',
+    paidBy: 1,
     date: '2020-01-12T18:20:00.000Z',
-    sum: 203,
+    sumTotal: 203,
+    sumIndividual: 203,
     status: 'accepted'
   },
   {
     id: 5,
     name: 'Теремок',
+    paidBy: 1,
     date: '2020-01-12T18:20:00.000Z',
-    sum: 213,
+    sumTotal: 213,
+    sumIndividual: 213,
     status: 'canceled'
   },
   {
     id: 6,
     name: 'Лента',
+    paidBy: 1,
     date: '2020-01-10T22:01:00.000Z',
-    sum: 366,
+    sumTotal: 366,
+    sumIndividual: 366,
     status: 'accepted'
   }
 ];
--- src/app/const/mock/check-list-debit.ts
+++ src/app/const/mock/check-list-debit.ts
@@ -4,50 +4,64 @@ export const CHECK_LIST_DEBIT: Check[] = [
   {
     id: 122,
     name: 'Лента',
+    paidBy: 1,
     date: '2019-12-19T17:12:00.000Z',
-    sum: 181,
+    sumTotal: 181,
+    sumIndividual: 181,
     status: 'draft'
   },
   {
     id: 123,
     name: 'Лента',
+    paidBy: 1,
     date: '2019-12-18T20:24:00.000Z',
-    sum: 1034,
+    sumTotal: 1034,
+    sumIndividual: 1034,
     status: 'accepted'
   },
   {
     id: 124,
     name: 'ОКЕЙ',
+    paidBy: 1,
     date: '2019-12-16T18:30:00.000Z',
-    sum: 431.31,
+    sumTotal: 431.31,
+    sumIndividual: 431.31,
     status: 'accepted'
   },
   {
     id: 125,
     name: 'ОКЕЙ',
+    paidBy: 1,
     date: '2019-12-16T18:30:00.000Z',
-    sum: 431.31,
+    sumTotal: 431.31,
+    sumIndividual: 431.31,
     status: 'canceled'
   },
   {
     id: 1012,
     name: 'telepizza',
+    paidBy: 1,
     date: '2019-12-13T22:29:00.000Z',
-    sum: 213,
+    sumTotal: 213,
+    sumIndividual: 213,
     status: 'accepted'
   },
   {
     id: 216,
     name: 'Лента',
+    paidBy: 1,
     date: '2019-12-11T20:19:00.000Z',
-    sum: 366,
+    sumTotal: 366,
+    sumIndividual: 366,
     status: 'accepted'
   },
   {
     id: 366,
     name: 'Лента',
+    paidBy: 1,
     date: '2019-12-10T19:21:00.000Z',
-    sum: 366,
+    sumTotal: 366,
+    sumIndividual: 366,
     status: 'accepted'
   }
 ];
