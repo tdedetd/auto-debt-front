6915c5df6ea4acaddf242d83ddcba92572104aad
tdedetd <tdedetd@gmail.com>
2020-04-05 21:40:03 +0300

    summary screen: normalize debts

--- src/app/screens/debt-summary/debt-summary.component.ts
+++ src/app/screens/debt-summary/debt-summary.component.ts
@@ -65,12 +65,14 @@ export class DebtSummaryComponent implements OnInit, OnDestroy {
       .subscribe(debtItems => {
         this.debtSum.unnormalized.credit = debtItems.reduce((add, item) => add + item.sum, 0);
         this.userItems.unnormalized.credit = debtItems;
+        this.updateNormalizedDebt();
       });
 
     this.summaryDebitSubscription = this.api.getSummaryDebit()
       .subscribe(debtItems => {
         this.debtSum.unnormalized.debit = debtItems.reduce((add, item) => add + item.sum, 0);
         this.userItems.unnormalized.debit = debtItems;
+        this.updateNormalizedDebt();
       });
 
     this.userInfoSubscription = this.userService.getUserInfo()
@@ -93,6 +95,48 @@ export class DebtSummaryComponent implements OnInit, OnDestroy {
     } else {
       this.selectedType = debtType;
     }
+  }
+
+  private updateNormalizedDebt() {
+    const usersCredit = this.userItems.unnormalized.credit;
+    const usersDebit = this.userItems.unnormalized.debit;
+    const normalizedUsersCredit: DebtSummaryItem[] = [];
+    const normalizedUsersDebit: DebtSummaryItem[] = [];
+
+    const repeatingUsersCredit = usersCredit.filter(debtItemCredit => {
+      return !!usersDebit.find(debtItemDebit => debtItemDebit.userId === debtItemCredit.userId);
+    });
+
+    normalizedUsersCredit.push(...usersCredit.filter(debtItemCredit => {
+      return !usersDebit.find(debtItemDebit => debtItemDebit.userId === debtItemCredit.userId);
+    }));
+
+    normalizedUsersDebit.push(...usersDebit.filter(debtItemDebit => {
+      return !usersCredit.find(debtItemCredit => debtItemCredit.userId === debtItemDebit.userId);
+    }));
+
+    repeatingUsersCredit.forEach(debtItemCredit => {
+      const debtItemDebit = usersDebit.find(debtItemDebit => debtItemDebit.userId === debtItemCredit.userId);
+      if (debtItemCredit.sum > debtItemDebit.sum) {
+        normalizedUsersCredit.push({
+          userId: debtItemCredit.userId,
+          username: debtItemCredit.username,
+          sum: debtItemCredit.sum - debtItemDebit.sum
+        });
+      } else {
+        normalizedUsersDebit.push({
+          userId: debtItemDebit.userId,
+          username: debtItemDebit.username,
+          sum: debtItemDebit.sum - debtItemCredit.sum
+        });
+      }
+    });
+
+    this.debtSum.normalized.credit = normalizedUsersCredit.reduce((add, item) => add + item.sum, 0);
+    this.debtSum.normalized.debit = normalizedUsersDebit.reduce((add, item) => add + item.sum, 0);
+
+    this.userItems.normalized.credit = normalizedUsersCredit.sort((a, b) => b.sum - a.sum);
+    this.userItems.normalized.debit = normalizedUsersDebit.sort((a, b) => b.sum - a.sum);
   }
 
 }
