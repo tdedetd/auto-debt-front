4da8649e41dd636226601ccedbe2a96d020d97d6
tdedetd <tdedetd@gmail.com>
2020-06-22 01:31:34 +0300

    edit-debt screen: participants list

--- src/app/screens/edit-debt/edit-debt.component.ts
+++ src/app/screens/edit-debt/edit-debt.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 import { Action } from 'src/app/components/status-bar-bottom/status-bar-bottom.component';
 
@@ -13,9 +15,31 @@ export class EditDebtComponent implements OnInit {
     
   ];
 
-  constructor() { }
+  checkId: number;
+
+  colors = ['#B7ADEF', '#FAB0B0', '#FACD85', '#75DE91', '#EE8AF2', '#74D9CE'];
+
+  edit = true;
+
+  faPlus = faPlus;
+
+  faTrash = faTrash;
+
+  participants: any = [
+    { name: 'Дмитрий', sum: 259.66 },
+    { name: 'Алексей', sum: 24.82 },
+    { name: 'Сергей', sum: 62.57 },
+    { name: 'Александр', sum: 74.81 }
+  ];
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.checkId = +this.activatedRoute.snapshot.params.checkId;
+
+    this.participants.forEach((user, i) => {
+      user.color = this.colors[i % this.colors.length];
+    });
   }
 
 }
