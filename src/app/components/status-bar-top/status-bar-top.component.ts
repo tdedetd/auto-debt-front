a4a4e0b4c66db81b86d371b9f14ff26f2b0f30eb
tdedetd <tdedetd@gmail.com>
2020-08-15 01:55:42 +0300

    subscribe -> async

--- src/app/components/status-bar-top/status-bar-top.component.ts
+++ src/app/components/status-bar-top/status-bar-top.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { UserService } from 'src/app/services/user.service';
 import { UserInfo } from 'src/app/models/user-info';
@@ -11,24 +11,17 @@ import { UserInfo } from 'src/app/models/user-info';
   templateUrl: './status-bar-top.component.html',
   styleUrls: ['./status-bar-top.component.css']
 })
-export class StatusBarTopComponent implements OnInit, OnDestroy {
+export class StatusBarTopComponent implements OnInit {
 
   faChevronLeft = faChevronLeft;
 
-  userInfo: UserInfo;
-
-  userInfoSubscription: Subscription;
+  userInfo$: Observable<UserInfo>;
 
   constructor(private location: Location,
               private userService: UserService) { }
 
   ngOnInit() {
-    this.userInfoSubscription = this.userService.getUserInfo()
-      .subscribe(userInfo => this.userInfo = userInfo);
-  }
-
-  ngOnDestroy() {
-    this.userInfoSubscription.unsubscribe();
+    this.userInfo$ = this.userService.getUserInfo();
   }
 
   onBackButtonClick() {
